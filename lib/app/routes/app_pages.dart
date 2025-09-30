ea093e0bc02b5b0e0b47d7afc5d5e6846d20c83f
devOmedia <syedhasan.cse@gmail.com>
2025-09-30 12:37:41 +0600

    ADDED: ONBOARDING MODULE

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,10 +1,11 @@
 import 'package:get/get.dart';
 
-import '../core/middlewares/login_middleware.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/login/bindings/login_binding.dart';
 import '../modules/login/views/login_view.dart';
+import '../modules/onboarding/bindings/onboarding_binding.dart';
+import '../modules/onboarding/views/onboarding_view.dart';
 import '../modules/register/bindings/register_binding.dart';
 import '../modules/register/views/register_view.dart';
 import '../modules/splash/bindings/splash_binding.dart';
@@ -38,6 +39,11 @@ class AppPages {
       name: _Paths.REGISTER,
       page: () => const RegisterView(),
       binding: RegisterBinding(),
+    ),
+    GetPage(
+      name: _Paths.ONBOARDING,
+      page: () => const OnboardingView(),
+      binding: OnboardingBinding(),
     ),
   ];
 }
