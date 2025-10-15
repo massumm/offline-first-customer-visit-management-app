a3e747523f0d354fae41a36f585d2281c516dd21
Mahamudul Hasan <syedhasan.cse@gmail.com>
2025-10-15 23:24:02 +0600

    HANDLED TRAINEE ONBOARDING UI

--- lib/app/modules/trainee_onboarding/controllers/trainee_onboarding_controller.dart
+++ lib/app/modules/trainee_onboarding/controllers/trainee_onboarding_controller.dart
@@ -1,24 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:icon/app/base/base_controller.dart';
 
 class TraineeOnboardingController extends BaseController {
-  //TODO: Implement TraineeOnboardingController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
+  final TextEditingController messageTextCtr = TextEditingController();
 
   @override
   void onClose() {
+    messageTextCtr.clear();
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/trainee_onboarding/views/trainee_onboarding_view.dart
+++ lib/app/modules/trainee_onboarding/views/trainee_onboarding_view.dart
@@ -1,24 +1,50 @@
 import 'package:flutter/material.dart';
 
 import 'package:icon/app/base/base_view.dart';
+import 'package:icon/app/core/widgets/input_widgets/adaptive_text_field.dart';
 
+import '../../icon_chat/views/icon_chat_view.dart';
 import '../controllers/trainee_onboarding_controller.dart';
 
 class TraineeOnboardingView extends BaseView<TraineeOnboardingController> {
   TraineeOnboardingView({super.key});
 
   @override
-  Widget body(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('TraineeOnboardingView'),
-        centerTitle: true,
+  PreferredSizeWidget? appBar(BuildContext context) {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(70),
+      child: CustomAppBar(),
+    );
+  }
+
+  @override
+  Widget? bottomNavigationBar(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8).copyWith(
+        bottom: MediaQuery.of(context).viewInsets.bottom + 8,
+        right: 8
       ),
-      body: const Center(
-        child: Text(
-          'TraineeOnboardingView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      child: Row(
+        children: [
+          Expanded(
+            child: AdaptiveSuperTextField(
+              controller: controller.messageTextCtr,
+              hintText: 'Type Here...',
+              onChanged: (value) {},
+            ),
+          ),
+          IconButton(onPressed: () {}, icon: Icon(Icons.send)),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget body(BuildContext context) {
+    return const Center(
+      child: Text(
+        'TraineeOnboardingView is working',
+        style: TextStyle(fontSize: 20),
       ),
     );
   }
