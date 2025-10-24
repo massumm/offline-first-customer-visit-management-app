4aa13d8467f9c6e5cf64411885ddd24f4040a667
Mahamudul Hasan <syedhasan.cse@gmail.com>
2025-10-24 20:30:10 +0600

    HANDLED EMAIL VALIDATIONs FOR TRAINEE FITNESS REPORT

--- lib/app/modules/trainee_fitness_report_generation/views/trainee_fitness_report_generation_view.dart
+++ lib/app/modules/trainee_fitness_report_generation/views/trainee_fitness_report_generation_view.dart
@@ -14,103 +14,101 @@ class TraineeFitnessReportGenerationView
 
   @override
   Widget body(BuildContext context) {
-    return SafeArea(
-      child: Stack(
-        children: [
-          // Background SVG
-          // TODO: Handle BG effect. Not working.
-          // Positioned(
-          //   top: -113,
-          //   left: -109,
-          //   child: Opacity(
-          //     opacity:
-          //         0.3, // Adjust opacity value (0.0 = transparent, 1.0 = opaque)
-          //     child: SvgPicture.asset(
-          //       Assets.svgBgGradientColor,
-          //       fit: BoxFit.cover,
-          //       width: 655.32,
-          //       height: 503.33,
-          //       // alignment: Alignment.topCenter,
-          //     ),
-          //   ),
-          // ),
-          // Content
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                BackPill(onTap: Get.back),
-                30.height,
-                RichText(
-                  text: TextSpan(
-                    style: Get.textTheme.bodyMedium?.copyWith(height: 2.0),
-                    children: [
-                      TextSpan(
-                        text: 'To receive your\n',
-                        style: Get.textTheme.titleLarge?.copyWith(),
-                      ),
-                      TextSpan(
-                        text: 'personalized Icon ',
-                        style: Get.textTheme.titleLarge?.copyWith(
-                          color: Get.theme.colorScheme.primary,
-                        ),
-                      ),
-                      TextSpan(
-                        text: 'Report,\n',
-                        style: Get.textTheme.titleLarge,
-                      ),
-                      TextSpan(
-                        text: 'please ',
-                        style: Get.textTheme.titleLarge,
-                      ),
-                      TextSpan(
-                        text: 'enter your',
-                        style: Get.textTheme.titleLarge?.copyWith(
-                          color: Get.theme.colorScheme.primary,
-                        ),
-                      ),
-                      TextSpan(
-                        text: ' email.',
-                        style: Get.textTheme.titleLarge,
-                      ),
-                    ],
-                  ),
-                ),
-                const Spacer(),
-                // Text('Email', style: Get.textTheme.labelLarge),
-                // 10.height,
-                Obx(() {
-                  return AdaptiveSuperTextField(
-                    controller: controller.emailCtr,
-                    hintText: "abc@example.com",
-                    labelText: 'Email',
-                    keyboardType: TextInputType.emailAddress,
-                    textInputAction: TextInputAction.next,
-                    errorText: controller.emailError.value,
-                    onTapOutside: (_) => FocusScope.of(context).unfocus(),
-                    onChanged: (value) {
-                      controller.onEmailChanged(value);
-                    },
-                  );
-                }),
-                10.height,
-                Obx(
-                  () => SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: controller.isValidEmail.isTrue
-                          ? controller.onSubmitButtonPressed
-                          : null,
-                      child: const Text('Next'),
+    return Stack(
+      children: [
+        // Background SVG
+        // TODO: Handle BG effect. Not working.
+        // Positioned(
+        //   top: -113,
+        //   left: -109,
+        //   child: Opacity(
+        //     opacity:
+        //         0.3, // Adjust opacity value (0.0 = transparent, 1.0 = opaque)
+        //     child: SvgPicture.asset(
+        //       Assets.svgBgGradientColor,
+        //       fit: BoxFit.cover,
+        //       width: 655.32,
+        //       height: 503.33,
+        //       // alignment: Alignment.topCenter,
+        //     ),
+        //   ),
+        // ),
+        // Content
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              BackPill(onTap: Get.back),
+              30.height,
+              RichText(
+                text: TextSpan(
+                  style: Get.textTheme.bodyMedium?.copyWith(height: 2.0),
+                  children: [
+                    TextSpan(
+                      text: 'To receive your\n',
+                      style: Get.textTheme.titleLarge?.copyWith(),
                     ),
+                    TextSpan(
+                      text: 'personalized Icon ',
+                      style: Get.textTheme.titleLarge?.copyWith(
+                        color: Get.theme.colorScheme.primary,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'Report,\n',
+                      style: Get.textTheme.titleLarge,
+                    ),
+                    TextSpan(
+                      text: 'please ',
+                      style: Get.textTheme.titleLarge,
+                    ),
+                    TextSpan(
+                      text: 'enter your',
+                      style: Get.textTheme.titleLarge?.copyWith(
+                        color: Get.theme.colorScheme.primary,
+                      ),
+                    ),
+                    TextSpan(
+                      text: ' email.',
+                      style: Get.textTheme.titleLarge,
+                    ),
+                  ],
+                ),
+              ),
+              const Spacer(),
+              // Text('Email', style: Get.textTheme.labelLarge),
+              // 10.height,
+              Obx(() {
+                return AdaptiveSuperTextField(
+                  controller: controller.emailCtr,
+                  hintText: "abc@example.com",
+                  labelText: 'Email',
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  errorText: controller.emailError.value,
+                  onTapOutside: (_) => FocusScope.of(context).unfocus(),
+                  onChanged: (value) {
+                    controller.onEmailChanged(value);
+                  },
+                );
+              }),
+              10.height,
+              Obx(
+                () => SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: controller.isValidEmail.isTrue
+                        ? controller.onSubmitButtonPressed
+                        : null,
+                    child: const Text('Next'),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
