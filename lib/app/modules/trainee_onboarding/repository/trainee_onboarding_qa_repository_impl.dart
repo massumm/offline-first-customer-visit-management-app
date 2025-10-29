695a90145825e83b5292787d2376f7673a132838
Mahamudul Hasan <syedhasan.cse@gmail.com>
2025-10-29 13:11:19 +0600

    CREATED TRAINEE ONBOARDING QA REPOSITORY AND IMPLEMENTATION

---
+++ lib/app/modules/trainee_onboarding/repository/trainee_onboarding_qa_repository_impl.dart
@@ -0,0 +1,5 @@
+import 'package:icon/app/base/base_remote_source.dart';
+import 'traineer_onboarding_qa_repository.dart';
+
+class TraineeOnboardingQARepositoryImpl extends BaseRemoteSource
+    implements TraineeOnboardingQARepository {}
