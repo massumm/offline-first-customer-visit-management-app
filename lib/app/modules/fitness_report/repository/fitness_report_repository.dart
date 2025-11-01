50b2bc6a894f78698b56e4b5b303fb7caeb5916b
Mahamudul Hasan <syedhasan.cse@gmail.com>
2025-11-01 18:42:06 +0600

    ADDED FITNESS REPORT REPOSITORY METHODS

--- lib/app/modules/fitness_report/repository/fitness_report_repository.dart
+++ lib/app/modules/fitness_report/repository/fitness_report_repository.dart
@@ -3,4 +3,20 @@ abstract class FitnessReportRepository {
       Map<String, dynamic> data, {
         void Function(int, int)? onSendProgress,
       });
+
+  Future<void> fetchRecoveryGoals();
+  Future<void> fetchSchedulesTasks();
+  Future<void> fetchRecoveryStrategies();
+  Future<void> fetchRecoveryObjective();
+  Future<void> fetchNutritionStrategies();
+  Future<void> fetchNutritionObjectives();
+  Future<void> fetchNutritionGoals();
+  Future<void> fetchFitnessGoals();
+  Future<void> fetchFitnessPlan();
+  Future<void> fetchActivityTask();
+  Future<void> fetchActivityStrategies();
+  Future<void> fetchActivityObjective();
+  Future<void> fetchActivityGoalsObjective();
+  Future<void> fetchActivityFocus();
+
 }
--- lib/app/modules/fitness_report/repository/fitness_report_repository_impl.dart
+++ lib/app/modules/fitness_report/repository/fitness_report_repository_impl.dart
@@ -30,4 +30,88 @@ class FitnessReportRepositoryImpl extends BaseRemoteSource
       rethrow;
     }
   }
+
+  @override
+  Future<void> fetchActivityFocus() {
+    // TODO: implement fetchActivityFocus
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fetchActivityGoalsObjective() {
+    // TODO: implement fetchActivityGoalsObjective
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fetchActivityObjective() {
+    // TODO: implement fetchActivityObjective
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fetchActivityStrategies() {
+    // TODO: implement fetchActivityStrategies
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fetchActivityTask() {
+    // TODO: implement fetchActivityTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fetchFitnessGoals() {
+    // TODO: implement fetchFitnessGoals
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fetchFitnessPlan() {
+    // TODO: implement fetchFitnessPlan
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fetchNutritionGoals() {
+    // TODO: implement fetchNutritionGoals
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fetchNutritionObjectives() {
+    // TODO: implement fetchNutritionObjectives
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fetchNutritionStrategies() {
+    // TODO: implement fetchNutritionStrategies
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fetchRecoveryGoals() {
+    // TODO: implement fetchRecoveryGoals
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fetchRecoveryObjective() {
+    // TODO: implement fetchRecoveryObjective
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fetchRecoveryStrategies() {
+    // TODO: implement fetchRecoveryStrategies
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> fetchSchedulesTasks() {
+    // TODO: implement fetchSchedulesTasks
+    throw UnimplementedError();
+  }
 }
