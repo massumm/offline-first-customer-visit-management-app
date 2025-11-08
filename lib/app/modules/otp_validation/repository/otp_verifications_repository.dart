608c68cf5f10aad40256cd75f82e6d1869eba073
Mahamudul Hasan <syedhasan.cse@gmail.com>
2025-11-08 19:38:37 +0600

    ADDED OTP VERIFICATION REPOSITORY

---
+++ lib/app/modules/otp_validation/repository/otp_verifications_repository.dart
@@ -0,0 +1,7 @@
+import 'package:icon/app/modules/login/models/login_response_model.dart';
+
+abstract class OtpVerificationsRepository{
+
+  Future<LoginResponseModel> varifyOtp(Map<String, dynamic> payload);
+
+}
