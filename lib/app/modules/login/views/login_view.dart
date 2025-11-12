5b85b1515e5a7e70346cfecee1b1d9233e3b552a
Mahamudul Hasan <syedhasan.cse@gmail.com>
2025-11-12 20:51:23 +0600

    MODIFIED

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:get/get.dart';
 import 'package:icon/app/base/base_view.dart';
