ea84be16fd13134e2375cece857a8b3cec840326
Mahamudul Hasan <syedhasan.cse@gmail.com>
2025-11-23 21:03:57 +0600

    HANDLE ROUTING

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -174,7 +174,7 @@ class LoginController extends BaseController {
         arguments: {'email': emailCtr.text},
       );
     } else {
-      Get.offAndToNamed(Routes.HOME);
+      Get.offAndToNamed(Routes.ICON_CHAT);
     }
   }
 
