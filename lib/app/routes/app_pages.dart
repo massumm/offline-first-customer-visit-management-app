e3d5afc7310beeceffed3d51cc29ad8361a22be7
Mahamudul Hasan <syedhasan.cse@gmail.com>
2025-12-06 16:08:32 +0600

    MODIFIED ROUTE

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -57,7 +57,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const String INITIAL = Routes.TRAINEE_ONBOARDING_BY_PAGE;
+  static const String INITIAL = Routes.SPLASH;
 
   static final routes = [
     GetPage(
