ae20c47e154662cfbbe0c3010b055f6a357ed0d2
Mahamudul Hasan <syedhasan.cse@gmail.com>
2025-12-07 21:32:06 +0600

    FIXED LAYOUT ISSUES

--- lib/app/modules/goal_tracking/views/goal_tracking_view.dart
+++ lib/app/modules/goal_tracking/views/goal_tracking_view.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
 import 'package:get/get.dart';
 import 'package:icon/app/core/extensions/app_extansions.dart';
-
-import 'package:icon/app/core/widgets/goal_stepper_footer.dart';
 import 'package:icon/app/core/widgets/super_image.dart';
-import 'package:icon/generated/assets.dart';
-
-import '../../your_activity_goals/views/your_activity_goals_view.dart';
+import '../../../../generated/assets.dart';
 import '../controllers/goal_tracking_controller.dart';
 
 class GoalTrackingView extends GetView<GoalTrackingController> {
@@ -41,98 +36,101 @@ class GoalTrackingView extends GetView<GoalTrackingController> {
               ),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: Get.mediaQuery.padding.top + 16),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                      vertical: 8,
-                    ),
-                    child: SizedBox(
-                      width: 40,
-                      height: 40,
-                      child: OutlinedButton(
-                        onPressed: () {},
-                        style: OutlinedButton.styleFrom(
-                          side: const BorderSide(color: Colors.white),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
+          SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: Get.mediaQuery.padding.top + 16),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 8,
+                      ),
+                      child: SizedBox(
+                        width: 40,
+                        height: 40,
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            side: const BorderSide(color: Colors.white),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            padding: const EdgeInsets.all(8),
                           ),
-                          padding: const EdgeInsets.all(8),
+                          child: SvgPicture.asset('assets/svg/arrow-left.svg'),
                         ),
-                        child: SvgPicture.asset('assets/svg/arrow-left.svg'),
                       ),
                     ),
+                  ],
+                ),
+                const SizedBox(height: 24),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          'Goal Tracking',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 36,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              const SizedBox(height: 24),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        'Goal Tracking',
-                        textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 6),
+                const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 8,
+                  ),
+                  child: Text(
+                    'No more app juggling. Everything is now in one place - convenient and effective.',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+                (Get.height * 0.45).height,
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 24),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 16,
+                  ),
+                  decoration: BoxDecoration(
+                    color: const Color(0x66AE2400),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SvgPicture.asset('assets/svg/tick.svg'),
+                      const SizedBox(height: 8),
+                      const Text(
+                        'Not every day is the same. This is where our adaptive engine kicks in, modifying your daily goals based on your life',
                         style: TextStyle(
                           color: Colors.white,
-                          fontSize: 36,
-                          fontWeight: FontWeight.w600,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 6),
-              const Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 24,
-                  vertical: 8,
-                ),
-                child: Text(
-                  'No more app juggling. Everything is now in one place - convenient and effective.',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
+                    ],
                   ),
                 ),
-              ),
-              (Get.height * 0.45).height,
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 24),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 16,
-                ),
-                decoration: BoxDecoration(
-                  color: const Color(0x66AE2400),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SvgPicture.asset('assets/svg/tick.svg'),
-                    const SizedBox(height: 8),
-                    const Text(
-                      'Not every day is the same. This is where our adaptive engine kicks in, modifying your daily goals based on your life',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+                const SizedBox(height: 24),
+              ],
+            ),
           ),
         ],
       ),
