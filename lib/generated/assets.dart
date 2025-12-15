efa7e7f9aa7f4d2aa23d10f285738ddd9dcaf251
syed <syedhasan.cse@gmail.com>
2025-12-15 22:37:21 +0600

    Refactor: Update activity log buttons and add new icons
    - Replace `_BottomButton` with `_LogActivityButton` for better naming clarity in the activity log widget.
    - Update the styling for the selected and unselected states of the activity buttons.
    - Add new SVG icons for `workout`, `cardiogram`, and `yoga`.
    - Regenerate `assets.dart` to include the new icon assets.

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -87,7 +87,10 @@ class Assets {
   static const String iconProfileSecond = 'assets/images/icon_profile/second.png';
   static const String iconProfileThird = 'assets/images/icon_profile/third.png';
   static const String iconsArrowUpLeft = 'assets/svg/icons/arrow-up-left.svg';
+  static const String iconsCardiogram = 'assets/svg/icons/cardiogram.svg';
   static const String iconsUpperBody = 'assets/svg/icons/upper_body.svg';
+  static const String iconsWorkout = 'assets/svg/icons/workout.svg';
+  static const String iconsYoga = 'assets/svg/icons/yoga.svg';
   static const String imagesApple = 'assets/images/apple.png';
   static const String imagesBgGradiemt = 'assets/images/bg-gradiemt.png';
   static const String imagesBgGradientCrop = 'assets/images/bg-gradient-crop.png';
